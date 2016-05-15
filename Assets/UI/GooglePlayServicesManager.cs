e2c4d517db8979ac36f21a67847b8ead79cba23b
Teodor Toshkov <teodor.toshkov95@gmail.com>
2016-05-15 20:00:03 +0300

    Integrated the basese of Google Play Services (login)

---
+++ Assets/UI/GooglePlayServicesManager.cs
@@ -0,0 +1,34 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine;
+using System.Collections;
+
+public class GooglePlayServicesManager : MonoBehaviour
+{	
+	// Use this for initialization
+	void Start ()
+	{
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+		PlayGamesPlatform.InitializeInstance(config);
+		// Activate the Google Play Games platform
+		PlayGamesPlatform.Activate();
+		// Authenticate
+		Debug.Log ("Authenticating...");
+
+		PlayGamesPlatform.Instance.Authenticate(SignInCallback, PlayGamesPlatform.Instance.localUser.authenticated);
+	}
+
+	private void SignInCallback(bool success)
+	{
+		if (success)
+		{
+			Debug.Log ("Signed In: " + PlayGamesPlatform.Instance.localUser.userName);
+		}
+		else
+		{
+			Debug.Log ("Sign-in failed.");
+		}
+	}
+}
